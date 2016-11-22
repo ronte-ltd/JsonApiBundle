005545b593687401f89fdcac2f6c353229854da0
Sergey Kodentsov <soulkoden@gmail.com>
2016-11-22 15:39:07 +0300

    Added JsonApiObjectRequest annotation

---
+++ Annotation/JsonApiObjectRequest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace RonteLtd\JsonApiBundle\Annotation;
+
+/**
+ * @author Sergey Kodentsov <soulkoden@gmail.com>
+ *
+ * @Annotation()
+ */
+class JsonApiObjectRequest
+{
+    /**
+     * @var string
+     */
+    public $class;
+}
